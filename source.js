d073b7e954ce0e8dca78b4b3f11e73572ee9a235
Ragan Harrison <42836738+raganh1229@users.noreply.github.com>
2018-10-21 18:53:13 +0800

    All HTML, CSS, and JS for current website.

---
+++ source.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+$(".show-more a").on("click", function() {
+    "use strict";
+	var $this = $(this); 
+    var $content = $this.parent().prev("div.content");
+    var linkText = $this.text().toUpperCase();    
+    
+    if(linkText === "SHOW MORE"){
+        linkText = "Show less";
+        $content.switchClass("hideContent", "showContent", 400);
+    } else {
+        linkText = "Show more";
+        $content.switchClass("showContent", "hideContent", 400);
+    }
+
+    $this.text(linkText);
+});
